666c5f78eecdaddd49cf57951633b5b482cab892
Abdullah <almahmuda5@gmail.com>
2023-07-07 15:33:42 +0600

    alert dialog

---
+++ app/src/main/java/com/cyberx/alertdialog/MainActivity.java
@@ -0,0 +1,61 @@
+package com.cyberx.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button;
+
+    private AlertDialog.Builder alertDialogBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.buttonId);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        // for setting title
+        alertDialogBuilder.setTitle(R.string.alert_tile);
+        //for setting message
+        alertDialogBuilder.setMessage(R.string.message);
+        //for setting icone
+        alertDialogBuilder.setIcon(R.drawable.question);
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "You have clicked cancel button", Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
